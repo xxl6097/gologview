15890aa7dd2e57e8887bd9885bf61e56ef37ecfd
MacBookAirM2 <uuxia@macbookairm2.com>
2023-05-11 21:00:26 +0800

    2023-05-11 21:00:26 by uuxia

--- go/logview/admin_api.go
+++ go/logview/admin_api.go
@@ -2,6 +2,7 @@ package logview
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"sort"
@@ -13,9 +14,13 @@ type GeneralResponse struct {
 	Msg  string
 }
 
-type FileResponse struct {
-	IsDir bool   `json:"isDir"`
-	Name  string `json:"name"`
+type TreeData struct {
+	Id       string `json:"id"`
+	Label    string `json:"label"`
+	Children []struct {
+		Id    string `json:"id"`
+		Label string `json:"label"`
+	} `json:"children,omitempty"`
 }
 
 func NewService() (svr *GeneralResponse) {
@@ -79,6 +84,11 @@ func (svr *GeneralResponse) ApiFiles(w http.ResponseWriter, r *http.Request) {
 		buf []byte
 	)
 
+	fmt.Println(w, "URL中的user请求参数的值是：", r.FormValue("path"))
+
+	fmt.Println(w, "你发送的请求地址是：", r.URL.Path)
+	fmt.Println(w, "你发送的请求地址后查询字符串是：", r.URL.RawQuery)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
 	// 必须，设置服务器支持的所有跨域请求的方法
@@ -90,16 +100,16 @@ func (svr *GeneralResponse) ApiFiles(w http.ResponseWriter, r *http.Request) {
 	// 可选，是否允许后续请求携带认证信息Cookir，该值只能是true，不需要则不设置
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-	dirs, err := os.ReadDir("/Users/uuxia/Desktop")
+	dirs, err := os.ReadDir(r.FormValue("path"))
 	if err != nil {
 		return
 	}
 
-	files := []FileResponse{}
+	files := []TreeData{}
 	for _, dir := range dirs {
-		f := FileResponse{
-			Name:  dir.Name(),
-			IsDir: dir.IsDir(),
+		f := TreeData{
+			Id:    dir.Name(),
+			Label: dir.Name(),
 		}
 		files = append(files, f)
 	}
@@ -109,3 +119,42 @@ func (svr *GeneralResponse) ApiFiles(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write(buf)
 	}()
 }
+
+/**
+[
+    {
+        "id": "node-1",
+        "label": "node-1",
+        "children": [
+            {
+                "id": "node-1-1",
+                "label": "node-1-1"
+            }
+        ]
+    },
+    {
+        "id": "node-2",
+        "label": "node-2",
+        "children": [
+            {
+                "id": "node-2-1",
+                "label": "node-2-1"
+            }
+        ]
+    },
+    {
+        "id": "node-3",
+        "label": "node-3",
+        "children": [
+            {
+                "id": "node-3-1",
+                "label": "node-3-1"
+            },
+            {
+                "id": "node-3-2",
+                "label": "node-3-2"
+            }
+        ]
+    }
+]
+*/
